9b1d68121bd31424bb9108bbfdee13061d4cc536
Danielyilma <yilmad752@gmail.com>
2023-11-16 17:29:32 +0300

    all monty project files

--- nop.c
+++ nop.c
@@ -9,12 +9,6 @@
 
 void _nop(stack_t **stack, unsigned int line_number)
 {
-	if (data.data != NULL)
-	{
-		fprintf(stderr, "L%u: usage: nop\n", line_number);
-		fclose(data.file);
-		exit(EXIT_FAILURE);
-	}
 	(void) stack;
 	(void) line_number;
 }
